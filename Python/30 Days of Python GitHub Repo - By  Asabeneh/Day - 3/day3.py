3878f17049c2ee481a53e00f6fe35dd9a7ddf7c9
AkshatFreeTimeNest <waadaiitka@gmail.com>
2025-11-05 12:56:04 +0530

    Add files via upload

---
+++ Python/30 Days of Python GitHub Repo - By  Asabeneh/Day - 3/day3.py
@@ -0,0 +1,99 @@
+# age = 18
+# height = 5.11
+# complex_number = 2 + 5j
+
+# #Area of triangle problem
+# base = float(input("Enter base: "))
+# height = float(input("Enter height: "))
+# area = 0.5 * base * height
+# print(area)
+
+# #Perimeter of triangle problem
+# side_a = float(input("Enter side a: "))
+# side_b = float(input("Enter side b: "))
+# side_c = float(input("Enter side c: "))
+# perimeter = side_a + side_b + side_c
+# print(perimeter)
+
+# #Rectangle Problem
+# length = float(input("Enter length: "))
+# width = float(input("Enter width: "))
+# area_rec = length * width
+# peri_rec = 2 * (length + width)
+# print(area_rec)
+# print(peri_rec)
+
+# #Circle Problem
+# rad = float(input("Enter radius: "))
+# pi = 3.14
+# area_cir = pi * rad * rad
+# circum = 2 * pi * rad
+# print(area_cir)
+# print(circum)
+
+# Comparing Problems
+# len_1 = len("python")
+# len_2 = len("dragon")
+# compar = len_1 != len_2
+# print(compar)
+
+# Error
+# op_and = "on" in "python" and "dragon"
+# print(op_and)
+
+# op_in = "I hope this course is not full of jargon."
+# opp = "jargon" in op_in
+# print(opp)
+
+# 16th problem
+# len_str = len("python")
+# text_float = float(len_str)
+# text_str = str(len_str)
+# print(text_float)
+# print(text_str)
+
+# 17 problem
+# calc = int(input("Enter the number: "))
+# cond = calc / 2 and calc % 2 == 0
+# print(cond)
+
+# 18 problem
+# floor_div = 7 // 3
+# output = floor_div == int(2.7)
+# print(output)
+
+# 19th prob
+# type_a = type('10')
+# type_b = type(10)
+# check = type_a == type_b
+# print(check)
+
+# #20 prob
+# int_1 = int(9.8)
+# int_2 = 10
+# check_int = int_1 == int_2
+# print(check_int)
+
+# 21 problem
+# hrs = float(input("Enter hours: "))
+# rate = float(input("Enter rate per hour: "))
+# weekly_earn = hrs * rate
+# print(weekly_earn)
+
+# 22 problem
+# years = int(input("Enter number of years you have lived: "))
+# living = years * 365 * 24 * 60 * 60
+# print(living)
+
+# 23 problem
+# print("1 1 1 1 1")
+# print("2 1 2 4 8")
+# print("3 1 3 9 27")
+# print("4 1 4 16 64")
+# print('5 1 5 25 125')
+
+#8th Problem
+# slope = 2
+# y_intercept = -2
+# x_intercept = 1
+# print("For y = 2x - 2:", "slope:", str(slope), "x - intercept:", str(x_intercept), "y - intercept:", y_intercept)
