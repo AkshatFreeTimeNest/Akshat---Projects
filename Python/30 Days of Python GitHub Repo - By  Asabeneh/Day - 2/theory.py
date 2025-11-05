79aa999409cc8c49204eb5a1b34138a3730cf671
AkshatFreeTimeNest <waadaiitka@gmail.com>
2025-11-05 12:55:06 +0530

    Add files via upload

---
+++ Python/30 Days of Python GitHub Repo - By  Asabeneh/Day - 2/theory.py
@@ -0,0 +1,30 @@
+print("Hello, World!") #1
+
+# len("Hello, World!") #len() is length function, which tells the number of characters in the brackets.
+
+type("Hello, World!") #Tells the type of data
+
+str(10)
+
+int(10)
+
+float(10)
+
+input("Enter your name: ")
+
+print(help("keywords")) #Gives the list of keywords
+
+print(help(str)) #Gives detailed info on the str function'''
+
+
+#max and min functions
+
+min(20, 30, 50, 100)
+
+max(40, 90, 000, 1002)
+
+min([20, 30, 40, 50]) #takes list as an argument and gives its min value
+
+max([90, 99, 11, 34, 988])
+
+sum([20, 30, 40, 90])
---
+++ Python/30 Days of Python GitHub Repo - By  Asabeneh/Day - 2/variables.py
@@ -0,0 +1,63 @@
+#Day 2: 30 Days of python programming
+
+#Excercise: Level 1
+
+first_name = "Abenese"
+last_name = "Bob"
+full_name = "Abenee Paul"
+country = "Europe"
+city = "Abilnase"
+age = 67
+year = 2008
+is_married = False
+is_true = False
+is_light_on = False
+address, contact, gender = "okatli resort", 990021999, "male"
+
+
+#Excercise: Level 2
+
+#Type Problem
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(address))
+print(type(contact))
+print(type(gender))
+
+#Length Problem
+print(len(first_name))
+print(len(last_name))
+num_one = 5
+num_two = 4
+total = (num_one + num_two)
+diff = (num_two - num_one)
+product = (num_one * num_two)
+division = (num_one / num_two)
+remainder = (num_two % num_one)
+exp = (num_one ** num_two)
+floor_division = (num_one // num_two)
+
+#Circle Problem
+radius_of_circle = 30
+area_of_circle = (3.14 * radius_of_circle * radius_of_circle)
+circum_of_circle = (2 * 3.14 * radius_of_circle)
+
+radius = float(input("enter radius: "))
+input_area = (3.14 * radius * radius)
+print(input_area)
+
+#Input Problem
+name = input("Enter the first name: ")
+last = input("Enter the last name: ")
+country = input("Enter your country: ")
+age = input("Enter your age: ")
+
+print("First name:", name,"," "Last name:", last,"," "Country:", country )
